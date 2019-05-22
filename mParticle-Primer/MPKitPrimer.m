a113e126c023b49bae1b73e5f202df32ed1aa4c7
Peter Jenkins <pjenkins@mparticle.com>
2019-05-22 14:29:13 -0400

    Auto-indent source code

--- mParticle-Primer/MPKitPrimer.m
+++ mParticle-Primer/MPKitPrimer.m
@@ -24,29 +24,29 @@
 
 - (MPKitExecStatus *)didFinishLaunchingWithConfiguration:(NSDictionary *)configuration {
     MPKitExecStatus *execStatus = nil;
-
+    
     if (![Primer isInitialized]) {
         NSLog(@"You must initialize the Primer SDK (e.g. using `startWithToken`) before starting mParticle!");
         execStatus = [[MPKitExecStatus alloc] initWithSDKCode:[[self class] kitCode] returnCode:MPKitReturnCodeRequirementsNotMet];
         return execStatus;
     }
-
+    
     _configuration = configuration;
-
+    
     [self start];
-
+    
     execStatus = [[MPKitExecStatus alloc] initWithSDKCode:[[self class] kitCode] returnCode:MPKitReturnCodeSuccess];
     return execStatus;
 }
 
 - (void)start {
-
+    
     static dispatch_once_t kitPredicate;
-
+    
     dispatch_once(&kitPredicate, ^{
-
+        
         self->_started = YES;
-
+        
         dispatch_async(dispatch_get_main_queue(), ^{
             NSDictionary *userInfo = @{mParticleKitInstanceKey: [[self class] kitCode]};
             [[NSNotificationCenter defaultCenter] postNotificationName:mParticleKitDidBecomeActiveNotification object:nil userInfo:userInfo];
@@ -55,82 +55,82 @@
 }
 
 - (id const)providerKitInstance {
-
+    
     return nil;
 }
 
 #pragma mark - Application
 
 - (nonnull MPKitExecStatus *)continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(void(^ _Nonnull)(NSArray * _Nullable restorableObjects))restorationHandler {
-
+    
     [Primer continueUserActivity:userActivity];
-
+    
     return [self statusWithCode:MPKitReturnCodeSuccess];
 }
 
 #pragma mark - User attributes and identities
 
 - (nonnull MPKitExecStatus *)setUserAttribute:(nonnull NSString *)key value:(nonnull NSString *)value {
-
+    
     if (!value) {
         return [self statusWithCode:MPKitReturnCodeFail];
     }
-
+    
     NSString *prefixedKey = [NSString stringWithFormat:@"mParticle.%@", key];
     [Primer appendUserProperties:@{prefixedKey: value}];
-
+    
     return [self statusWithCode:MPKitReturnCodeSuccess];
 }
 
 #pragma mark - e-Commerce
 
 - (nonnull MPKitExecStatus *)logCommerceEvent:(nonnull MPCommerceEvent *)commerceEvent {
-
+    
     MPKitExecStatus *status = [[MPKitExecStatus alloc] initWithSDKCode:[[self class] kitCode] returnCode:MPKitReturnCodeSuccess forwardCount:0];
-
+    
     NSArray *expandedInstructions = [commerceEvent expandedInstructions];
     for (MPCommerceEventInstruction *commerceEventInstruction in expandedInstructions) {
         [self logEvent:commerceEventInstruction.event];
         [status incrementForwardCount];
     }
-
+    
     return status;
 }
 
 #pragma mark - Events
 
 - (nonnull MPKitExecStatus *)logEvent:(nonnull MPEvent *)event {
-
+    
     NSMutableDictionary *parameters = [NSMutableDictionary dictionaryWithObject:@"mParticle" forKey:@"pmr_event_api"];
-
+    
     if (event.info) {
         [parameters addEntriesFromDictionary:event.info];
     }
-
+    
     [Primer trackEventWithName:event.name parameters:parameters];
-
+    
     return [self statusWithCode:MPKitReturnCodeSuccess];
 }
 
 - (nonnull MPKitExecStatus *)logScreen:(nonnull MPEvent *)event {
-
+    
     return [self logEvent:event];;
 }
 
 #pragma mark - Assorted
 
 - (nonnull MPKitExecStatus *)setDebugMode:(BOOL)debugMode {
-
+    
     PMRLoggingLevel loggingLevel = debugMode ? PMRLoggingLevelWarning : PMRLoggingLevelNone;
     [Primer setLoggingLevel:loggingLevel];
-
+    
     return [self statusWithCode:MPKitReturnCodeSuccess];
 }
 
 #pragma mark - Utilities
 
 - (nonnull MPKitExecStatus *)statusWithCode:(MPKitReturnCode)code {
-
+    
     return [[MPKitExecStatus alloc] initWithSDKCode:[[self class] kitCode] returnCode:code];
 }
 
